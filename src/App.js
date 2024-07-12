edf316397cb9aeefc683b34b17a48ea4eabba148
dharmendra-patel <91276987+Patel-tech@users.noreply.github.com>
2024-07-12 10:44:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/component/Navigation/Navigation';
+
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        Home
+      </div>
+    </div>
+  );
+}
+
+export default App;
